2f4cf3561e1e039cfc6e0a951c16be196ebc8e3a
Ahmedhossam <ahmedhossamdev1@gmail.com>
2023-05-19 06:42:50 +0200

    fix whitelist error

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -17,7 +17,7 @@ mongoose.connect(DB, {
 
 //Read JSON FILE
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // IMPORT DATA INTO DB
 
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
-
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -91,8 +91,41 @@ const tourSchema = new mongoose.Schema({
   secretTour: {
     type: Boolean,
     default: false
-  }
-}, {
+  },
+  startLocation:{
+    // GeoJSON
+    type: {
+      type: String,
+      enum: ['Point'],
+      default: 'Point',
+    },
+    coordinates: [Number],
+    address : String,
+    description : String,
+  },
+  locations : [
+    {
+      type : {
+        type: String,
+        default: 'Point',
+        enum:['Point']
+      },
+      coordinates: [Number],
+      address : String,
+      description : String,
+      day:Number,
+    },
+  ],
+  guides: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  ]
+
+},
+
+  {
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
 });
@@ -101,10 +134,16 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+
 //DOCUMENT MIDDLEWARE : run before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   console.log(this);
+  next();
+});
+tourSchema.pre('save' , async function(next) {
+  const guidesPromises  = this.guides.map(async id => await User.findById(id))
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
@@ -124,12 +163,20 @@ tourSchema.post('save', function(doc, next) {
 //   next();
 // });
 
+tourSchema.pre(/^find/ , function(next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
+  next();
+});
 
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
   next();
 });
+
 
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`QUERY TOOK ${Date.now() - this.start} milliseconds`);
