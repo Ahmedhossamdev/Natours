a8eabaa4a9e13978220b77a1a0e9818d973f97ec
Ahmedhossam <ahmedhossamdev1@gmail.com>
2023-05-24 09:44:41 +0300

    Factory Functions

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,39 +2,39 @@
 
 
 const mongoose = require('mongoose');
-
+const Tour = require('./tourModel');
 const reviewSchema = new mongoose.Schema({
-   review:{
-     type:String,
-     required:[true, 'Review can not be empty!']
-   },
-   rating:{
-     type:Number,
-     min:1,
-     max:5
-   },
-   createdAt:{
-     type:Date,
-     default:Date.now(),
-   },
-   tour:{
-     type:mongoose.Schema.ObjectId,
-     ref:'Tour',
-     required: [true, 'Review must belong to a tour.']
-   },
-   user:{
-     type:mongoose.Schema.ObjectId,
-     ref:'User',
-     required: [true, 'review must belong to a user.']
-   },
-},
-{
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true }
-});
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'review must belong to a user.']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  });
 
 // TODO search about populate , photo
-reviewSchema.pre(/^find/ , function(next) {
+reviewSchema.pre(/^find/, function(next) {
   // this.populate({
   //   path: 'tour',
   //   select: 'name'
@@ -49,10 +49,38 @@ reviewSchema.pre(/^find/ , function(next) {
       path: 'user',
       select: 'name photo'
     }
-  )
+  );
   next();
 });
 
 
+// Calculate the statistics if new Reviews are added
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage:  stats[0].avgRating
+  });
+};
+
+reviewSchema.post('save', function() {
+  // This point to the current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// Calculate Average Rating
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
