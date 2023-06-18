4dd397cdf84a7cb83f82ed7f842354c4704fd21a
Ahmedhossam <ahmedhossamdev1@gmail.com>
2023-06-18 21:17:33 +0300

    Factory Functions

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,7 +9,7 @@ export const bookTour = async tourId => {
     const stripe = new Stripe('pk_test_51NFrrJEYpHjuu9ZWnbjq2066vJzeI6DCSc76wV03fKXzA9A5wIop96KnTM4iXmf3Qqg2VctogoKm89MP8KIi86k600RnZVMA0G');
 
     // 1) Get Checkout session from API
-    const session = await axios(`http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     console.log(session);
     // 2) Create checkout form + charge credit card
     // await stripe.redirectToCheckout({
